69fa52e24b5edcc088510407191efbb07fff76c1
Akono <akonobrathwaite@gmail.com>
2021-05-12 14:00:05 -0400

    Add /info command and refactor code

--- bot.js
+++ bot.js
@@ -3,58 +3,134 @@ const axios = require("axios");
 
 require("dotenv").config();
 
-
 // // replace the value below with the Telegram token you receive from @BotFather
-// const token = "1731872262:AAHV3Jf3-vdDYoaGhwuLv2JK4nqXg2js5Ns";
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
 
-async function getPriceData(cryptoSymbol){
-  const cryptoSymbolUpperCased =  cryptoSymbol.toUpperCase()
+async function getCryptoData(cryptoSymbol, command) {
+  const cryptoSymbolUpperCased = cryptoSymbol.toUpperCase();
+  let endPoint = "";
+  let params = {};
+  let data = {};
+
+  if (command === "priceData" || command === "infoData") {
+    endPoint = "/v1/cryptocurrency/quotes/latest";
+    params = {
+      symbol: cryptoSymbolUpperCased,
+    };
+  }
+
   const CMCApiQueryOptions = {
-    url: "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest",
+    url: `https://pro-api.coinmarketcap.com${endPoint}`,
     headers: {
       "X-CMC_PRO_API_KEY": process.env.X_CMC_PRO_API_KEY,
     },
-    params: {
-      symbol: cryptoSymbolUpperCased,
-    },
-    method:"get"
+    params,
+    method: "get",
   };
+
   try {
-    const data = await axios(CMCApiQueryOptions)
+    data = await axios(CMCApiQueryOptions);
+  } catch (error) {
+    return error;
+  }
+
+  if ((command === "priceData")) {
     const specificCryptoData = data.data.data[Object.keys(data.data.data)[0]];
-    const currentPriceInUSD = specificCryptoData.quote.USD.price.toFixed(2);
-    const percentageChange24hrs = specificCryptoData.quote.USD.percent_change_24h.toFixed(2);
+    const currentPriceInUSD = specificCryptoData.quote.USD.price.toLocaleString({
+      maximumFractionDigits: 4,
+    });
+    const percentageChange24hrs =
+      specificCryptoData.quote.USD.percent_change_24h.toFixed(2);
     const name = specificCryptoData.name;
     const symbol = specificCryptoData.symbol;
-
     return {
       currentPriceInUSD,
       percentageChange24hrs,
       name,
-      symbol
+      symbol,
     };
-
-  } catch (error) {
-    return error;
+  } else if ((command === "infoData")) {
+    const specificCryptoData = data.data.data[Object.keys(data.data.data)[0]];
+    const currentMarketCap =
+      specificCryptoData.quote.USD.market_cap.toLocaleString({
+        maximumFractionDigits: 2,
+      });
+    const volume24hr = specificCryptoData.quote.USD.volume_24h.toLocaleString({
+      maximumFractionDigits: 2,
+    });
+    const circulatingSupply =
+      specificCryptoData.circulating_supply.toLocaleString({
+        maximumFractionDigits: 2,
+      });
+    const name = specificCryptoData.name;
+    const symbol = specificCryptoData.symbol;
+    return {
+      currentMarketCap,
+      volume24hr,
+      circulatingSupply,
+      name,
+      symbol,
+    };
   }
 }
 
-
-// Capture text (should be cryptosymbol) that proceeds after the command /price "
+/* GET PRICE DATA FOLLOWING A '/price [cryptosymbol]' COMMAND */
+// Capture text (should be cryptosymbol e.g eth or ETH) that proceeds after the command /price "
 bot.onText(/\/price (.+)/, async (msg, match) => {
+  // message is message being sent to person querying bot
+  let message = "Sumn nuh right";
+  let command = "priceData";
+  let error = "";
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
-
   const chatId = msg.chat.id;
   const cryptoSymbol = match[1]; // the captured "cryptoCode"
 
-  const cryptoData = await getPriceData(cryptoSymbol);
+  const cryptoData = await getCryptoData(cryptoSymbol, command);
 
-  const message = `*${cryptoData.name} (${cryptoData.symbol})*: ${cryptoData.currentPriceInUSD} USD | ${cryptoData.percentageChange24hrs}% ${cryptoData.percentageChange24hrs > 0 ?  "increase" : "decrease"} 24hr`;
+  if (cryptoData === error) {
+    message = "Error...try again loser";
+  }
+
+  message = `*${cryptoData.name} (${cryptoData.symbol})*: ${
+    cryptoData.currentPriceInUSD
+  } USD | ${cryptoData.percentageChange24hrs}% ${
+    cryptoData.percentageChange24hrs > 0 ? "increase" : "decrease"
+  } (24hr)`;
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, message, { parse_mode: "MARKDOWN" });
+});
+
+//--------------------------------------------------------------------//
+/* GET CRYPTO INFO FOLLOWING A '/info [cryptosymbol]' COMMAND */
+// Capture text (should be cryptosymbol e.g eth or ETH) that proceeds after the command /price "
+bot.onText(/\/info (.+)/, async (msg, match) => {
+  // message is message being sent to person querying bot
+  let message = "Sumn nuh right";
+  let command = "infoData";
+  let error = "";
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+  const chatId = msg.chat.id;
+  const cryptoSymbol = match[1]; // the captured "cryptoCode"
+
+  const cryptoData = await getCryptoData(cryptoSymbol, command);
+
+  if (cryptoData === error) {
+    message = "Error...try again loser";
+  }
+
+  message = `*${cryptoData.name} (${cryptoData.symbol})*: 
+ *Marketcap*: ${
+    cryptoData.currentMarketCap
+  } USD | 
+ *Volume (24hr):* ${cryptoData.volume24hr} USD |  
+ *Circulating Supply*: ${cryptoData.circulatingSupply} coins`;
 
   // send back the matched "whatever" to the chat
   bot.sendMessage(chatId, message, { parse_mode: "MARKDOWN" });
